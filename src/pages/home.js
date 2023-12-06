40e739ad260ffc82e94f7b7dcc8de1c52f3acbb1
Danny <lorddarcness@hotmail.com>
2023-12-06 12:56:21 +0000

    Navbar/Homepage/App routes created

---
+++ src/pages/home.js
@@ -0,0 +1,18 @@
+import './home.css';
+
+import {Container} from 'react-bootstrap';
+
+
+
+function HomePage(){
+    
+    return(
+           <div className='home-index'>
+                <Container>
+                    <h1>Home Page Test</h1>
+               </Container>
+            </div>
+    );  
+}
+
+export default HomePage;
