5e0a1478d54a6922dc2081736ddf27411e9b05d4
ElCapitan <dee.jay.bhx@gmail.com>
2023-12-12 11:45:12 +0000

    BG colour issue fixed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import HomePage from './pages/home';
+
+import NavigationBar from './components/navigation/NavigationBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <NavigationBar />
+        <Routes>
+          <Route index element={<HomePage />} />
+          {/*
+          <Route path="add" element={<AddTools />} />
+          <Route path="updatePage" element={<UpdatePage />} />
+          <Route path="update">
+            <Route path=":toolId" element={<UpdateTool />} />
+          </Route>
+          <Route path="delete" element={<DeleteTool />} />
+          */}
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/navigation/NavigationBar.js
@@ -0,0 +1,53 @@
+import './NavigationBar.css';
+
+import React from 'react';
+import {Navbar, Container, Nav, Offcanvas, NavDropdown} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
+
+function NavigationBar() {
+    return(
+        <div className='navigation-bar'>
+            <h1>banner placeholder</h1>
+            <>
+            {[false].map((expand) => (
+                <Navbar key={expand} expand={expand} className="bg-body-tertiary mb-3">
+                 <Container className='navigation-bar' fluid>
+                 <Navbar.Toggle className='navigation-bar' aria-controls={`offcanvasNavbar-expand-${expand}`} />
+                 <Navbar.Brand className='navigation-bar' as={Link} to="/">Ultimate KPOP Database</Navbar.Brand>
+                 <Navbar.Offcanvas
+                      id={`offcanvasNavbar-expand-${expand}`}
+                      aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+                      placement="start"
+                 >
+                <Offcanvas.Header className='navigation-bar' closeButton>
+                <Offcanvas.Title className='navigation-bar' id={`offcanvasNavbarLabel-expand-${expand}`}>
+                    Ultimate KPOP Database
+                </Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body className='navigation-bar'>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                    <Nav.Link as={Link} to="/">Home</Nav.Link>
+                    <Nav.Link as={Link} to="/">Groups</Nav.Link>
+                    <Nav.Link as={Link} to="/">Idols</Nav.Link>
+                    <Nav.Link as={Link} to="/">Videos</Nav.Link>
+                    <NavDropdown title="Admin" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="/">Groups</NavDropdown.Item>
+                        <NavDropdown.Item href="/">Idols</NavDropdown.Item>
+                        <NavDropdown.Item href="/">Videos</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="/">
+                         Separated link
+                        </NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+                </Offcanvas.Body>
+                </Navbar.Offcanvas>
+                </Container>
+            </Navbar>
+        ))}
+        </>
+        </div>
+    )
+}
+
+export default NavigationBar;
