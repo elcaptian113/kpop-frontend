4ebdd43d183b3cf56a1fb97653492697982083fc
Danny <lorddarcness@hotmail.com>
2023-12-30 10:44:05 +0000

    removed grid view

--- src/pages/home.js
+++ src/pages/home.js
@@ -11,18 +11,10 @@ function HomePage(){
                 <Container>
                     <h1>Ultimate KPOP Database</h1>
                     <br></br>
-                    <Container>
-                        <Row>
-                            <Col sm={4}>
                                 <img src="https://www.rollingstone.com/wp-content/uploads/2023/07/kpop100-list-taegeuk.jpg?w=1581&amp;h=1054&amp;crop=1"  
-                                     style={{ height: "30vmin"}} 
+                                     style={{ height: "50vmin"}} 
                                      alt="The 100 Best Korean Pop Songs Of All Time" />
-                            </Col>
-                            <Col sm={8}>
                                 <p>Welcome to the Ultimate KPOP Database!</p>
-                            </Col>
-                        </Row>
-                    </Container>
                </Container>
             </div>
     );  
