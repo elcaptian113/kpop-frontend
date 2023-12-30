b64f910686e458c140ee5cb06a856eea8179d283
Danny <lorddarcness@hotmail.com>
2023-12-30 10:32:36 +0000

    navbar cleanup

--- src/components/navigation/NavigationBar.js
+++ src/components/navigation/NavigationBar.js
@@ -1,7 +1,8 @@
 import './NavigationBar.css';
 import logo from './logo.JPG';
+import brandLogo from './brandLogo.jpg';
 import React from 'react';
-import {Navbar, Container, Nav, Offcanvas, NavDropdown} from 'react-bootstrap';
+import {Navbar, Container, Nav, NavDropdown} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
 
 function NavigationBar() {
@@ -11,22 +12,12 @@ function NavigationBar() {
             <img src={logo} className="App-logo" alt="logo" />
             <>
             {[false].map((expand) => (
-                <Navbar key={expand} expand={expand} className="mb-3">
+                <Navbar collapseOnSelect expand="lg" className="mb-3">
                  <Container className='navigation-bar' fluid>
-                 <Navbar.Brand className='navigation-bar' as={Link} to="/"></Navbar.Brand>
+                 <Navbar.Brand className='navigation-bar' as={Link} to="/"><img src={brandLogo} alt="Brand Logo"/></Navbar.Brand>
                  <Navbar.Toggle className='navigation-bar' aria-controls={`offcanvasNavbar-expand-${expand}`} />
-                 <Navbar.Offcanvas
-                      id={`offcanvasNavbar-expand-${expand}`}
-                      aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-                      placement="end"
-                 >
-                <Offcanvas.Header className='navigation-bar' closeButton>
-                <Offcanvas.Title className='navigation-bar' id={`offcanvasNavbarLabel-expand-${expand}`}>
-                    Ultimate KPOP Database
-                </Offcanvas.Title>
-                </Offcanvas.Header>
-                <Offcanvas.Body className='navigation-bar'>
-                <Nav className="justify-content-end flex-grow-1 pe-3">
+                 <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="justify-content-start flex-grow-1 pe-3">
                     <Nav.Link as={Link} to="/">Home</Nav.Link>
                     <Nav.Link as={Link} to="/groups">Groups</Nav.Link>
                     <Nav.Link as={Link} to="/idols">Idols</Nav.Link>
@@ -35,8 +26,7 @@ function NavigationBar() {
                         <NavDropdown.Item href="/idolsAdmin">Idols</NavDropdown.Item>
                     </NavDropdown>
                 </Nav>
-                </Offcanvas.Body>
-                </Navbar.Offcanvas>
+                </Navbar.Collapse>
                 </Container>
             </Navbar>
         ))}
